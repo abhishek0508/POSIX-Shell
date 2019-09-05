e3210a0404a53bc44d4aa5f4bd320166acf8d370
Abhishek <abhishek.gorisaria@alienbrains.in>
2019-09-05 23:19:25 +0530

    history of command on up arrow key

--- addhistory.h
+++ addhistory.h
@@ -1,8 +1,9 @@
 #include "utils.h"
 using namespace std;
+vector<string> vec;
 void adhistory(string history){
-
+    vec.push_back(history);
 }
-void print_history(){
-    
+string print_history(int i){
+    return vec.at(i);
 }
